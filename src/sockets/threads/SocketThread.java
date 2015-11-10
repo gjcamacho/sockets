9ebc7dd7ca8591c25f3dd6081004f40b34b62510
Gerardo Camacho <gjcamacho@hotmail.com>
2015-11-10 13:30:26 -0600

    Se agrego la parte de servidor web

--- src/sockets/threads/SocketThread.java
+++ src/sockets/threads/SocketThread.java
@@ -11,6 +11,7 @@ public class SocketThread extends Thread{
 
 	private Socket sfd;
 	private int id;
+	private boolean end;
 
 	public SocketThread(Socket sfd, int id) {
 		this.sfd=sfd;
@@ -26,15 +27,36 @@ public class SocketThread extends Thread{
 		
 		
 			PrintWriter pw=new PrintWriter(new OutputStreamWriter(sfd.getOutputStream()));
-			pw.println("El hilo " + this.id + " te da la bienvenida");
-			pw.flush();
-			System.out.println(bf.readLine());
-		
-		
+			//pw.println("El hilo " + this.id + " te da la bienvenida");
+			//pw.flush();
 			
-			pw.println("Dato recibido...");
+			while(!end){
+				String entrada="";
+				//System.out.println(entrada);
+				
+				while(bf.ready()){
+					
+					entrada=bf.readLine();
+					System.out.println(entrada);
+				}
+				
+				String html="<html><body>Ya deja de conectarte <a href='http://google.com'>Javier</a>!!</body></html>";
+				
+				pw.println("HTTP/1.0 200 OK");
+				pw.println("Date: Fri, 31 Dec 1999 23:59:59 GMT");
+				pw.println("Content-Type: text/html");
+				pw.println("Content-Length: "+ html.length() +"");
+				pw.println("\n");
+				pw.println(html);
+				
+				if(entrada.lastIndexOf("finalizar")>-1){
+					this.end=true;
+				}
+				
+				pw.flush();
+			}
+			System.out.println("Adios...");
 			
-			pw.flush();
 			pw.close();
 			bf.close();
 			sfd.close();
